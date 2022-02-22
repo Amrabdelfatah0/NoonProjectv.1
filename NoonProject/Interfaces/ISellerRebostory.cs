907af8f314d2cafe55ae99e11693365dadd02bb5
Amr Mohamed <88450778+Amrabdelfatah0@users.noreply.github.com>
2022-02-22 19:17:51 +0200

    v.2

---
+++ NoonProject/Interfaces/ISellerRebostory.cs
@@ -0,0 +1,20 @@
+﻿using NoonProject.Models.Seller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoonProject.Interfaces
+{
+    public class ISellerRebostory
+
+    {
+        void Add(Seller obj);
+        void Update(Seller obj);
+        void Delet(int Id);
+        List<Seller> Get();
+
+        Seller Get(int ID);
+    }
+}
