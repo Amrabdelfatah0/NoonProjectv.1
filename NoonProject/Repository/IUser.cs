8a9fba6c270e34e06c015acb0e9de7e764dd9f20
Amr Mohamed <88450778+Amrabdelfatah0@users.noreply.github.com>
2022-02-28 23:14:58 +0200

    v.6

---
+++ NoonProject/Repository/IUser.cs
@@ -0,0 +1,19 @@
+﻿using NoonProject.Models.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoonProject.Repository
+{
+    public interface IUserRepository : IGenericCRUD<User>
+    {
+        bool IsPasswordMatch(string Password, string ConfirmPassword);
+        User LogIn(string UserName, string Password);
+        bool IsUserValidate(User entity, bool IsUpdate = false);
+        bool IsUserNameExist(string UserName);
+        bool IsExist(int Id);
+        bool IsExist(string UserName, string Password);
+    }
+}
